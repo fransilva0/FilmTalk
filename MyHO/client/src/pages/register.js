76ebf6ef8af450577459a315dfca77dec79e30eb
TharcioCBM <camboim_@hotmail.com>
2024-04-04 17:24:56 -0300

    feat: some corrections and improvement of part of the validation method in the frontend
    Co-authored-by: Francileudo Oliveira <fransilva0@users.noreply.github.com>

--- MyHO/client/src/pages/register.js
+++ MyHO/client/src/pages/register.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{ useState }  from "react"
 import { HeaderLoginLogout } from "../components/Header"
 import { Input } from "../components/Input"
 import { DefaultButton } from "../components/Button"
@@ -19,25 +19,82 @@ const FormSection = styled.section`
 
 `;
 
+const InputStyled = styled.input`
+    color: #FFFFFF;
+    border: 2px solid #9F9F9F;
+    border-radius: 0.5rem;
+    outline: 0;
+    background: #181818;
+    margin: 0.5rem 0 0.5rem 0;
+    padding: 0.5rem;
+
+`;
+
 const ButtonSection = styled.section`
 
     display: flex;
     justify-content: right;
 `;
 
-export default function Login() {
+const CommonStyling = styled.button`
+    font-size: 1rem;
+    line-height: center;
+    padding: 0.5rem 2rem 0.5rem 2rem;
+    cursor: pointer;
+
+`;
+
+const ButtonregisterStyled = styled(CommonStyling)`
+    background: #DF2222;
+    border-radius: 0.5rem;
+    border: 4px solid #DF2222;
+    color: #000;
+    font-weight: bold;
+
+    &:hover {
+      background: transparent;
+      color: #fff;
+    }
+`;
+
+const DefaultButtonStyled = styled(ButtonregisterStyled)`
+    color: #fff;
+
+    &:hover {
+      background: transparent;
+      color: #fff;
+    }
+`;
+  
+export default function Register() {
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [mensage, setMensage] = useState('');
+
+  const CheckPassword = () => {
+    console.log(password)
+    console.log(repeatPassword)
+    if (password == repeatPassword) {
+      setMensage('As senhas são iguais!');
+    } else {
+      setMensage('As senhas são diferentes!');
+    }
+  }
+
   return (
     <>
       <HeaderLoginLogout />
       <FormSection>
         <h2>Registre-se</h2>
-        <Input placeholder="user name" type="text" />
-        <Input placeholder="password" type="password" />
-        <Input placeholder="repeat password" type="password" />
+        <InputStyled placeholder="user name" type="text" />
+        <InputStyled placeholder="E-mail" type="text" />
+        <InputStyled placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <InputStyled placeholder="repeat password" type="password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} />
         <ButtonSection>
-          <DefaultButton text="inscrever-se"/>
+          <DefaultButtonStyled type="submit" onClick={CheckPassword}>inscrever-se</DefaultButtonStyled>
         </ButtonSection>
       </FormSection>
+      <p>{mensage}</p>
 
     </>
   )
