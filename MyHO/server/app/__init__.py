ffcbbc36f3b7c0059b23534d80b719eca7dc2fb8
TharcioCBM <camboim_@hotmail.com>
2024-04-05 12:24:47 -0300

    feat: implementation of the migrate extension for database migrations.

--- MyHO/server/app/__init__.py
+++ MyHO/server/app/__init__.py
@@ -1,15 +1,17 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 def create_app():
     app = Flask(__name__,root_path=os.getcwd(), static_url_path='/static')
     app.config.from_object(Config)
     CORS(app)
-    from app.shared.dataBase import db
+    
+    from app.shared.dataBase import db,migrate
     db.init_app(app)
+    migrate.init_app(app, db)
+
     from app.routes.UserRoutes import user_bp
     app.register_blueprint(user_bp)
 
--- MyHO/server/app/shared/dataBase.py
+++ MyHO/server/app/shared/dataBase.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from flask_migrate import Migrate
 
 class Base(DeclarativeBase):
   pass
 
 db = SQLAlchemy(model_class=Base)
+
+migrate = Migrate()
