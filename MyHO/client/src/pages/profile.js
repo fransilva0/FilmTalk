989ffd958a85ae007bcebb59fe650068836f8117
fransilva0 <sfrancileudo1@gmail.com>
2024-04-07 16:06:12 -0300

    feat: creating profile screen and rendering for profile data configuration screen

---
+++ MyHO/client/src/pages/profile.js
@@ -0,0 +1,118 @@
+import React,{ useState }  from "react"
+import Image from 'next/image';
+import styled from 'styled-components';
+import imgProfile from "../assets/img-profile.jpg";
+import { MainHeader } from "../components/Header"
+import { SettingsScreen } from "../components/settingsScreen";
+import { Icon } from '@iconify/react';
+
+const ProfileSection = styled.section`
+
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+div {
+    margin-right: 0.5rem;
+
+    p {
+        color: #fff;
+        text-align: right;
+    }
+}
+
+img  {
+    border-radius: 5rem;
+}
+
+`;
+
+const ProfileFooter = styled.footer`
+
+    padding: 1rem 0.5rem 0.5rem 0.5rem;
+    border-top: 1px solid #DF2222;
+    display: flex;
+    flex-direction: none;
+    justify-content: center;
+
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+
+    button {
+        background: transparent;
+        border: none;
+        cursor: pointer;
+        padding: 0 0.2rem 0 0.2rem;
+    }
+
+    @media (min-width: 1025px) {
+        
+        button {
+            padding: 0 1rem 0 1rem;
+        }
+
+    }
+
+`;
+
+
+export default function Profile() {
+    const [Screen, setScreen] = useState('PublicationsProfile');
+
+    const ControlScreen = (dataScreen) => {
+        setScreen(dataScreen)
+    }
+
+    let renderContent;
+
+    if (Screen === 'PublicationsProfile') {
+        renderContent = <p>Tela de Publicações do Usuário</p>;
+    } else if (Screen === 'settings') {
+      renderContent = <SettingsScreen/>;
+    }
+
+    return (
+        <>
+            <MainHeader>
+
+                <ProfileSection>
+                    <div>
+                        <p>Username</p>
+                        <p>0 posts</p>
+                    </div>
+                    <Image src={imgProfile} alt="image by Carter Baran, via Unsplash" width="61" height="61" />
+                </ProfileSection>
+
+            </MainHeader>
+
+            {renderContent}
+
+            <ProfileFooter>
+                
+                <button>
+                    <Icon icon="mdi-users" style={{ color: '#fff', fontSize: '4rem', margin: "0", padding: "0" }} />
+                </button>
+                
+                <button>
+                    <Icon icon="fluent-movies-and-tv-20-filled" style={{ color: '#fff', fontSize: '4rem', margin: "0", padding: "0" }} />
+                </button>
+                
+                <button onClick={() => ControlScreen('PublicationsProfile')}>
+                    <Icon icon="ic:round-home" style={{ color: '#fff', fontSize: '4rem', margin: "0", padding: "0" }} />
+                </button>
+                
+                <button>
+                    <Icon icon="fluent-person-feedback-16-filled" style={{ color: '#fff', fontSize: '4rem', margin: "0", padding: "0" }} />
+                </button>
+                
+                <button onClick={() => ControlScreen('settings')}>
+                    <Icon icon="ic-baseline-settings" style={{ color: '#fff', fontSize: '4rem', margin: "0", padding: "0" }} />
+                </button>
+
+            </ProfileFooter>
+
+        </>
+    )
+    
+}
