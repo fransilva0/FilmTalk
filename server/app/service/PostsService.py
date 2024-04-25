13f5588b5514cf082c1dd95faa73ba591c797ae9
Tharcio Camboim <89159550+TharcioCBM@users.noreply.github.com>
2024-04-25 18:12:41 -0300

    Feature/scrum 18 (#11)
    * feat: functionality to request a new publication
    
    * feat: implementation of the function get posts from a user
    
    * feat: implementation of edit and delete a post functionalities

--- server/app/service/PostsService.py
+++ server/app/service/PostsService.py
@@ -2,7 +2,7 @@ from app.model.Posts import Posts
 from app.schemas.PostsSchema import PostsSchema
 from app.repository.PostsRepository import PostsRepository
 
-post = Posts(title=None,publication=None,user_id=None)
+
 postsSchema = PostsSchema()
 postsRepository = PostsRepository()
 
@@ -11,15 +11,34 @@ class PostsService:
     def __init__(self) -> None:
         pass
 
-    def add_new_publication(self,title,publication,user_id):
-        
+    def add_new_publication(self,title,publication,user_id):      
+        post = Posts(title=None,publication=None,user_id=None)
         post.title = title
         post.publication = publication
         post.user_id = user_id
         postsRepository.save(post)
+        print(post)
         return
+    
+    def update_publication(self,user_id,post_id,title,publication):
+        post = postsRepository.getById(post_id)
+        if post.user_id != user_id:
+            raise Exception("Você não tem permissão para editar esse post!", 403)
+        post.title = title
+        post.publication = publication
+        postsRepository.update(post)
+        return
+    
+    def delete_publication(self,user_id,post_id):
+        post = postsRepository.getById(post_id)
+        if post.user_id != user_id:
+            raise Exception("Você não tem permissão para deletar esse post!", 403)
+        postsRepository.delete(post)
+        return
+        
     def findPublicationById(self,id):
         ...
+        
     def findAllPublicationByUserId(self,user_id):
         publications = postsRepository.getAllByUserId(user_id)
         response = postsSchema.dump(publications,many=True)
@@ -27,3 +46,5 @@ class PostsService:
             for  publication in response:
                 publication.pop("user_id")
         return response
+
+
