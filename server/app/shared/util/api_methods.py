3f989f5370e63954237b1763a822c6cb749cebe5
TharcioCBM <camboim_@hotmail.com>
2024-05-10 16:04:04 -0300

    Revert "bug: Temporary workaround for get post view function"
    This reverts commit 67db7c94f18b352aaf5b70a85928b350ea38e8d1.

--- server/app/shared/util/api_methods.py
+++ server/app/shared/util/api_methods.py
@@ -1,7 +1,7 @@
 from app.service.UserService import UserService
 
 userService = UserService()
-
+#TODO:refatorar essa função
 def convert_posts_userid_to_username(json_page):
     for post in json_page:
         id = post.get("user_id")
@@ -9,6 +9,13 @@ def convert_posts_userid_to_username(json_page):
         user = userService.findUserById(id=id)
         post["username"] = user.username
     return json_page
+
+def convert_post_userid_to_username(post):
+    id = post.get("user_id")
+    user = userService.findUserById(id=id)
+    post.pop("user_id")
+    post["username"] = user.username
+    return post
 
 def paginate(result):
     ...
