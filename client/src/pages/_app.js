ce6aee6660e12552a6a75642d72e2e37afbe8af4
fransilva0 <sfrancileudo1@gmail.com>
2024-05-27 15:22:37 -0300

    fix: origin of the bug that showed semicolons at the end of screens discovered

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -3,6 +3,6 @@ import GlobalStyle from '../styles/global';
 export default function App({ Component, pageProps }) {
   return <>
     <GlobalStyle />
-    <Component {...pageProps} />;
+    <Component {...pageProps} />
   </>
 }
